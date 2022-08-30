1946b167c7f66f4582958149b855afefd70a23d2
Katherine Gower <kfgower@gmail.com>
2022-08-29 21:35:24 -0400

    added comments; modified playRound() function

--- JS/script.js
+++ JS/script.js
@@ -2,31 +2,20 @@
 let playerWins = 0
 let compWins = 0
 
-
 //evaluate rock paper scissors winner from player input and computer input
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         playerWins +=1;
         compWins +=1;
-        return `It's a tie! Your wins: ${playerWins}, Computer wins: ${compWins}`;
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return `It's a tie! Your score: ${playerWins}, Computer score: ${compWins}`;
+    } else if ((playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")
+    || (playerSelection == "rock" && computerSelection == "scissors")) {
         playerWins +=1;
-        return `You win! Paper beats rock. Your wins: ${playerWins}, Computer wins: ${compWins}`;
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return `You win! ${playerSelection} beats ${computerSelection}. Your score: ${playerWins}, Computer score: ${compWins}`;
+    } else if ((playerSelection == "scissors" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection == "paper")
+    || (playerSelection == "paper" && computerSelection == "scissors")) {
         compWins +=1;
-        return `You lose! Rock beats scissors. Your wins: ${playerWins}, Computer wins: ${compWins}`;
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        compWins +=1;
-        return `You lose! Paper beats rock. Your wins: ${playerWins}, Computer wins: ${compWins}`;
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        playerWins +=1;
-        return `You win! Scissors beat paper. Your wins: ${playerWins}, Computer wins: ${compWins}`;
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        playerWins +=1;
-        return `You win! Rock beats scissors. Your wins: ${playerWins}, Computer wins: ${compWins}`;
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        compWins +=1;
-        return `You lose! Scissors beat paper. Your wins: ${playerWins}, Computer wins: ${compWins}`;
+        return `You lose! ${computerSelection} beats ${playerSelection}. Your score: ${playerWins}, Computer score: ${compWins}`;
     } else {
         return "Invalid - try again";
     }
@@ -38,8 +27,10 @@ function end(){
         console.log(`Game over! You won! Good job!`);
     } else if (compWins > playerWins) {
         console.log(`Game over! The computer won! Better luck next time!`);
-    } else if (compWins == playerWins) {
+    } else if ((compWins == playerWins) && (compWins > 0 || playerWins > 0)) {
         console.log(`Game over! You tied! Good job!`);
+    } else {
+        console.log(`Game aborted. Refresh to start over.`)
     }
     }
 
@@ -65,14 +56,18 @@ function game() {
                 return "scissors";
             }
             }
-            console.log(getComputerChoice());
 
         //Create variable computerSelection with value being the output of getComputerChoice()
         const computerSelection = getComputerChoice();
-        console.log(computerSelection);
+
 
         //Ask user for input rock, paper, or scissors, and store in variable "playerSelection"        
-        playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+        playerSelection = prompt("Rock, paper, or scissors?")
+        if (playerSelection) {
+            playerSelection.toLowerCase();
+        } else {
+            break;
+        }
 
         //call playRound function and print result to console
         playRound(playerSelection, computerSelection);
@@ -81,38 +76,4 @@ function game() {
 end();
 }
 
-
-
 game()
-
-//create variable to count player wins
-//create variable to count computer wins
-//increment
-
-
-//Create function round to play 1 round of RPS; 
-    //if computerSelection === playerSelection, output "It's a tie!"
-
-    //if computerSelection === rock,
-        //if playerSelection === paper, output "You win! Paper beats rock."
-        //if playerSelection === scissors, output "You lose! Rock beats scissors."
-
-    //if computerSelection === paper,
-        //if playerSelection === rock, output "You lose! Paper beats rock."
-        //if playerSelection === scissors, output "You win! Scissors beat paper."
-
-    //if computerSelection === scissors,
-        //if playerSelection === rock, output "You win! Rock beats scissors."
-        //if playerSelection === paper, output "You lose! Scissors beat paper."
-
-
-  
-
-
-
-//Create function game to play a 5 round game that keeps score and reports a winner or loser at the end
-
-//begin with sentry at start and add change to sentry on each pass until sentry is larger than or equal to finish
-//when a user completes a round, add 1
-//if the number is < 5, continue loop
-//if number is >= 5, print winner and exit.
